6042364add9f4e139f6d59c322fa9483d0f14606
Bezlonir <michael_bragg99@hotmail.com>
2018-03-28 19:43:15 -0600

    Complete jQuery for basic track search by title.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -15,18 +15,47 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  for (i = 0; i < tracks.results.trackmatches.track.length; i++) {
+    var thisTitle = tracks.results.trackmatches.track[i].name;
+    var thisArtist = tracks.results.trackmatches.track[i].artist;
+    var thisPic = tracks.results.trackmatches.track[i].image[1]["#text"];
+    var thisListeners = tracks.results.trackmatches.track[i].listeners;
+    var searchResult =
+      '<div id="search-results" class="row">' +
+        '<div class="col-xs-1 col-xs-offset-1">' +
+          '<i class="fa fa-play-circle-o"></i>' +
+        '</div>' +
+        '<div class="song-title col-xs-4">' +
+          '<span>' + thisTitle
+          + '</span>' +
+        '</div>' +
+        '<div class="artist col-xs-2">' +
+          '<span>' + thisArtist
+          + '</span>' +
+        '</div>' +
+        '<div class="artwork col-xs-2">' +
+          '<img src="' + thisPic +'"/>' +
+        '</div>' +
+        '<div class="popularity col-xs-2">' +
+          '<span>' + thisListeners
+          + '</span>' +
+        '</div>' +
+      '</div>';
 
+      $('#search-container').append(searchResult);
+  }
 };
 
 /*
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
+  Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+API_KEY+"&format=json",
     datatype:'jsonp',
-    success: function() {
-      console.log("data pulled");}
+    success: function(track_data) {
+      Trackster.renderTracks(track_data);
+    }
 });
 };
